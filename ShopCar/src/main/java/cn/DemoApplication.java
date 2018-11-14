283e132e87e738424a64e9ce0e6a52e128baf2a9
root <1849246592@qq.com>
2018-11-14 08:25:33 +0800

    yml

---
+++ ShopCar/src/main/java/cn/DemoApplication.java
@@ -0,0 +1,16 @@
+package cn;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@MapperScan("mapper")
+@EnableSwagger2
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
