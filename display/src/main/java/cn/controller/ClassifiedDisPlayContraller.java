73e2ff03299d84d3ad57718d36afafbc0ea17df9
sjq19940827 <15803431215@163.com>
2018-11-15 10:15:44 +0800

    display

--- display/src/main/java/cn/controller/ClassifiedDisPlayContraller.java
+++ display/src/main/java/cn/controller/ClassifiedDisPlayContraller.java
@@ -2,7 +2,6 @@ package cn.controller;
 
 import cn.service.ClassifiedDisplayService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
